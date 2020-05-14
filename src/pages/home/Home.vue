d8a95cc0e9e04f2790f988afaf6c750eee451978
SakuraAyane66 <809653361@qq.com>
2020-05-14 13:18:35 +0800

    好久的重新学习，全力找工作

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,19 @@
 <template>
 <div>
-    <div>home</div>
+    
+    <home-header></home-header>
     <router-link to="/list">list</router-link>
+    <div>home</div>
 </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: { 
+      HomeHeader
+  }
 }
 </script>
 
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="header">
+  <div class="header-left">返回</div>
+  <div class="header-input">输入城市/景点/游玩主体</div>
+  <div class="header-right">城市</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style scoped>
+     
+    .header{
+        line-height: 40px;
+        display: flex;
+        background-color: aqua;
+        font-size: 10px;
+    }  
+    .header-left{
+       width: 60px;
+       display: flex;
+       float: left;
+       background-color: red;
+    }    
+    .header-input{
+        flex: 1;
+        background-color: #fff;
+        height: 30px;
+        line-height: 30px;
+        margin-top: 5px;
+        border-radius: 5px;
+        margin-left: 12px;
+        color: #ccc;
+    }
+    .header-right{
+        width: 100px;
+        display: flex;
+        float: right;
+        background-color: red;
+        text-align: center;
+    }
+      
+</style>
