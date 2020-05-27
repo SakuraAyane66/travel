59df8f0769d1a9917a55b462f1e350e03aa7c806
SakuraAyane66 <809653361@qq.com>
2020-05-28 00:52:05 +0800

    暂定了学习日语

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,8 +3,9 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
-    <router-link to="/list">list</router-link>
-    <div>home</div>
+    <home-recommend></home-recommend>
+    <!--<router-link to="/list">list</router-link>
+    <div>home</div>-->
 </div>
 </template>
 
@@ -12,12 +13,14 @@
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/swiper'
 import HomeIcons from './components/icons'
+import HomeRecommend from './components/Recommend'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeRecommend
   }
 }
 </script>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+    <ul>
+        <li class="item">
+            <div class="item-img-wrapper">
+                <img class="item-img" src='' />
+            </div>
+            <p></p>
+            <p></p>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend'
+}
+</script>
+
+<style scoped>
+.recommend-title{
+    line-height: 40px;
+    background: #87CEEB;
+    text-indent: .2rem;
+    margin-top: .2rem;
+}
+</style>
