91b97c5dca87b7a13238f9e91656dd6bbd826324
SakuraAyane66 <809653361@qq.com>
2020-05-30 18:49:48 +0800

    search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="search">
+      <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style scoped>
+  .search{
+    height: 2rem;
+    background: aqua;
+    padding: 0.1rem;
+  }
+  .search-input{
+    padding:0 .1rem;
+    height: 1.2rem;
+    text-align: center;
+    line-height: 1.2rem;
+    width: 98%;
+    border-radius: .06rem;
+    color: #666;
+  }
+</style>
