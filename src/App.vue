aec42a38d80fefd9c59273d2e108453c9875e4b7
SakuraAyane66 <809653361@qq.com>
2020-06-01 11:20:51 +0800

    wa

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <!--<img src="./assets/logo.png">-->
-    <router-view/><!--显示的是当前路由地址对应的内容-->
+    <keep-alive>
+      <router-view/><!--显示的是当前路由地址对应的内容-->
+    </keep-alive>
   </div>
 </template>
 
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -17,6 +17,7 @@ import HomeIcons from './components/icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 export default {
   name: 'Home',
   components: {
@@ -28,15 +29,19 @@ export default {
   },
   data () {
     return {
+      lastCity: '',
       swiperList: [],
       iconList: [],
       recommendList: [],
       weekendList: []
     }
   },
+  computed: {
+    ...mapState(['city'])
+  },
   methods: {
     getHomeInfo () {
-      axios.get('/api/index.json')
+      axios.get('/api/index.json?city=' + this.city)
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
@@ -52,7 +57,16 @@ export default {
     }
   },
   mounted () {
+    this.lastCity = this.city
+    console.log('mounted')
     this.getHomeInfo()
+  },
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
+    console.log('activited')
   }
 }
 </script>
